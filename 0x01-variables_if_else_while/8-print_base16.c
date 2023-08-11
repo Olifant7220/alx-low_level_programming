1b34551c5d35c4d83baec30fcc2b2f2f978fe125
Olifant <mphoolifant7220@gmail.com>
2023-08-11 14:10:29 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all the numbers of base 16 in lowercase,
+ * main - print all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
@@ -18,3 +18,4 @@ putchar(ch);
 }
 putchar('\n');
 return (0);
+}
